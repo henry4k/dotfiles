b271e87baac8c10017205943ebc4ebd541a290bc
Henry Kielmann <H.Kielmann@Laudert.de>
2013-12-12 18:15:12 +0100

    Inital commit.

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+set -e
+
+$(dirname $0)/uninstall.sh
+
+Dotfiles="$PWD/$(dirname $0)"
+
+cat >> "$HOME/.profile" << EOF
+# DOTFILES BEGIN
+source '$Dotfiles/profile'
+# DOTFILES END
+EOF
+
+cat >> "$HOME/.zshrc" << EOF
+# DOTFILES BEGIN
+source '$Dotfiles/zshrc'
+# DOTFILES END
+EOF
+
+cat >> "$HOME/.vimrc" << EOF
+" DOTFILES BEGIN
+source $Dotfiles/vimrc
+" DOTFILES END
+EOF
+
+cat >> "$HOME/.tmux.conf" << EOF
+# DOTFILES BEGIN
+source-file "$Dotfiles/tmux.conf"
+# DOTFILES END
+EOF
+
+echo 'Installed dotfiles!'
---
+++ uninstall.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+set -e
+
+RandomPrefix=$(basename $0)
+
+function UninstallDotfile()
+{
+    local file="$1"
+
+    if [ ! -e $file ]; then
+        return
+    fi
+
+    local tempFile="/tmp/$RandomPrefix-$RANDOM"
+    awk -f - $file > $tempFile << "EOF"
+/.*DOTFILES BEGIN.*/, /.*DOTFILES END.*/ { next }
+{ print $0 }
+EOF
+
+    mv -f $tempFile $file
+
+    echo "Uninstalled dotfiles from $file"
+}
+
+UninstallDotfile "$HOME/.profile"
+UninstallDotfile "$HOME/.zshrc"
+UninstallDotfile "$HOME/.vimrc"
+UninstallDotfile "$HOME/.tmux.conf"
+
+echo 'Uninstalled dotfiles!'
