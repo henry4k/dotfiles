0b275ffd86e96f55a97be0bb92861f64dc9758b5
Henry Kielmann <H.Kielmann@Laudert.de>
2014-03-28 18:15:14 +0100

    Made git up more informative..

--- install.sh
+++ install.sh
@@ -50,7 +50,7 @@ $GitConfig mergetool.fugitive.cmd "vim -f -c 'Gdiff' \$MERGED"
 $GitConfig alias.d 'difftool'
 $GitConfig alias.ctags '!.git/hooks/ctags'
 $GitConfig alias.lg "log --color --abbrev-commit --pretty=format:'%C(bold black)%h by %C(reset)%C(blue)%an %C(bold black)%ar: %C(reset)%s'"
-$GitConfig alias.up "!f() { git fetch \$@ && git lg --reverse HEAD@{1}..; }; f"
+$GitConfig alias.up "!f() { git fetch --verbose \$@ && git lg --reverse HEAD@{1}..; }; f"
 
 "$Dotfiles/dircolors.sh" > "$Destination/.dircolors"
 
