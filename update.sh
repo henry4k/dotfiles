8ffa58980e202ae1bb977c5fc2f7db1d62c757f6
Henry Kielmann <H.Kielmann@Laudert.de>
2014-07-04 17:14:21 +0200

    Thesaurus word list is downloaded automatically.

--- update.sh
+++ update.sh
@@ -23,3 +23,40 @@ popd
 pushd vim/bundle
 GitCloneOrPull 'vundle' 'https://github.com/gmarik/vundle.git'
 popd
+
+
+function DownloadAndExtractZip()
+{
+    local dir="$1"
+    local url="$2"
+
+    if [[ -e "$dir" ]]; then
+        echo "$dir has already been downloaded"
+    else
+        mkdir -p "$dir"
+        local tmp="${RANDOM}.zip"
+        curl --progress-bar "$url" -o "$tmp"
+        unzip -d "$dir" "$tmp"
+        rm "$tmp"
+    fi
+}
+
+function DownloadAndExtractZipFile()
+{
+    local file="$1"
+    local url="$2"
+
+    if [[ -e "$file" ]]; then
+        echo "$file has already been downloaded"
+    else
+        local tmp="${RANDOM}.zip"
+        curl --progress-bar "$url" -o "$tmp"
+        unzip "$tmp" "$file"
+        rm "$tmp"
+    fi
+}
+
+mkdir -p vim/dictionaries
+pushd vim/dictionaries
+DownloadAndExtractZipFile 'mthesaur.txt' 'http://www.gutenberg.org/dirs/etext02/mthes10.zip'
+popd
